7be432f06d0217c85f155faba6b0d00300523113
ciny2312 <ciny2312@sjtu.edu.cn>
2024-10-11 22:09:41 +0800

    update builtin.s

--- builtin.s
+++ builtin.s
@@ -162,3 +162,149 @@ ptr.substring:                          # @ptr.substring
 	.globl	ptr.parseInt                    # -- Begin function ptr.parseInt
 	.p2align	1
 	.type	ptr.parseInt,@function
+ptr.parseInt:                           # @ptr.parseInt
+# %bb.0:
+	addi	sp, sp, -16
+	sw	ra, 12(sp)                      # 4-byte Folded Spill
+	lui	a1, %hi(.L.str.1)
+	addi	a1, a1, %lo(.L.str.1)
+	addi	a2, sp, 8
+	call	sscanf
+	lw	a0, 8(sp)
+	lw	ra, 12(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 16
+	ret
+.Lfunc_end9:
+	.size	ptr.parseInt, .Lfunc_end9-ptr.parseInt
+                                        # -- End function
+	.globl	ptr.ord                         # -- Begin function ptr.ord
+	.p2align	1
+	.type	ptr.ord,@function
+ptr.ord:                                # @ptr.ord
+# %bb.0:
+	add	a0, a0, a1
+	lbu	a0, 0(a0)
+	ret
+.Lfunc_end10:
+	.size	ptr.ord, .Lfunc_end10-ptr.ord
+                                        # -- End function
+	.globl	builtin.getSize                 # -- Begin function builtin.getSize
+	.p2align	1
+	.type	builtin.getSize,@function
+builtin.getSize:                        # @builtin.getSize
+# %bb.0:
+	lw	a0, -4(a0)
+	ret
+.Lfunc_end11:
+	.size	builtin.getSize, .Lfunc_end11-builtin.getSize
+                                        # -- End function
+	.globl	builtin.printBool               # -- Begin function builtin.printBool
+	.p2align	1
+	.type	builtin.printBool,@function
+builtin.printBool:                      # @builtin.printBool
+# %bb.0:
+	addi	sp, sp, -16
+	lui	a1, %hi(.L.str.3)
+	addi	a1, a1, %lo(.L.str.3)
+	sw	a1, 8(sp)                       # 4-byte Folded Spill
+	lui	a1, %hi(.L.str.4)
+	addi	a1, a1, %lo(.L.str.4)
+	sw	a1, 12(sp)                      # 4-byte Folded Spill
+	beqz	a0, .LBB12_2
+# %bb.1:
+	lw	a0, 8(sp)                       # 4-byte Folded Reload
+	sw	a0, 12(sp)                      # 4-byte Folded Spill
+.LBB12_2:
+	lw	a0, 12(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 16
+	tail	printf
+.Lfunc_end12:
+	.size	builtin.printBool, .Lfunc_end12-builtin.printBool
+                                        # -- End function
+	.globl	builtin.allocArray              # -- Begin function builtin.allocArray
+	.p2align	1
+	.type	builtin.allocArray,@function
+builtin.allocArray:                     # @builtin.allocArray
+# %bb.0:
+	addi	sp, sp, -16
+	sw	ra, 12(sp)                      # 4-byte Folded Spill
+	sw	a0, 8(sp)                       # 4-byte Folded Spill
+	addi	a0, a0, 1
+	li	a1, 4
+	call	calloc
+	lw	a1, 8(sp)                       # 4-byte Folded Reload
+	sw	a1, 0(a0)
+	addi	a0, a0, 4
+	lw	ra, 12(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 16
+	ret
+.Lfunc_end13:
+	.size	builtin.allocArray, .Lfunc_end13-builtin.allocArray
+                                        # -- End function
+	.globl	builtin.stringConcatenate       # -- Begin function builtin.stringConcatenate
+	.p2align	1
+	.type	builtin.stringConcatenate,@function
+builtin.stringConcatenate:              # @builtin.stringConcatenate
+# %bb.0:
+	addi	sp, sp, -32
+	sw	ra, 28(sp)                      # 4-byte Folded Spill
+	sw	a1, 16(sp)                      # 4-byte Folded Spill
+	sw	a0, 12(sp)                      # 4-byte Folded Spill
+	call	strlen
+	mv	a1, a0
+	lw	a0, 16(sp)                      # 4-byte Folded Reload
+	sw	a1, 8(sp)                       # 4-byte Folded Spill
+	call	strlen
+	lw	a1, 8(sp)                       # 4-byte Folded Reload
+	add	a0, a0, a1
+	sw	a0, 20(sp)                      # 4-byte Folded Spill
+	addi	a0, a0, 1
+	call	malloc
+	lw	a1, 12(sp)                      # 4-byte Folded Reload
+	sw	a0, 24(sp)                      # 4-byte Folded Spill
+	call	strcpy
+	lw	a1, 16(sp)                      # 4-byte Folded Reload
+                                        # kill: def $x12 killed $x10
+	lw	a0, 24(sp)                      # 4-byte Folded Reload
+	call	strcat
+	lw	a1, 20(sp)                      # 4-byte Folded Reload
+                                        # kill: def $x12 killed $x10
+	lw	a0, 24(sp)                      # 4-byte Folded Reload
+	add	a2, a0, a1
+	li	a1, 0
+	sb	a1, 0(a2)
+	lw	ra, 28(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 32
+	ret
+.Lfunc_end14:
+	.size	builtin.stringConcatenate, .Lfunc_end14-builtin.stringConcatenate
+                                        # -- End function
+	.type	.L.str,@object                  # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"%s"
+	.size	.L.str, 3
+
+	.type	.L.str.1,@object                # @.str.1
+.L.str.1:
+	.asciz	"%d"
+	.size	.L.str.1, 3
+
+	.type	.L.str.2,@object                # @.str.2
+.L.str.2:
+	.asciz	"%d\n"
+	.size	.L.str.2, 4
+
+	.type	.L.str.3,@object                # @.str.3
+.L.str.3:
+	.asciz	"true"
+	.size	.L.str.3, 5
+
+	.type	.L.str.4,@object                # @.str.4
+.L.str.4:
+	.asciz	"false"
+	.size	.L.str.4, 6
+
+	.ident	"Ubuntu clang version 18.1.8 (++20240731024944+3b5b5c1ec4a3-1~exp1~20240731145000.144)"
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
